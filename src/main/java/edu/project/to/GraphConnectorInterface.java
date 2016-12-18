97eb1aa95320a90788fbcced5cdea9ecefd91623
magda <magdalenastachon26@gmail.com>
2016-12-18 23:12:00 +0100

    Provide interface to fetch GEXF files

---
+++ src/main/java/edu/project/to/GraphConnectorInterface.java
@@ -0,0 +1,11 @@
+package edu.project.to;
+
+import java.io.File;
+
+/**
+ * Created by onegrx on 18.12.16.
+ */
+
+public interface GraphConnectorInterface {
+    File getGraphFile(String name);
+}
---
+++ src/main/java/edu/project/to/GraphModuleConnector.java
@@ -0,0 +1,27 @@
+package edu.project.to;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created by onegrx on 18.12.16.
+ */
+public class GraphModuleConnector implements GraphConnectorInterface {
+
+    @Override
+    public File getGraphFile(String name) {
+        List<Person> personList = DbConnector.getPeopleByName(name);
+
+        //TODO
+
+        Object object = createGexfFile(personList);
+
+        return null;
+    }
+
+    //TODO refactor this
+    private Object createGexfFile(List<Person> personList) {
+        return new Object();
+    }
+
+}
