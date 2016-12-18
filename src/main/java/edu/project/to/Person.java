be25dc325f250f15f64767601f2bd37d4961a2d4
magda <magdalenastachon26@gmail.com>
2016-12-18 22:26:37 +0100

    Refactor Person

--- src/main/java/edu/project/to/Person.java
+++ src/main/java/edu/project/to/Person.java
@@ -3,28 +3,29 @@ package edu.project.to;
 /**
  * Created by magda on 17.12.16.
  */
+
 public class Person {
-    private String Name;
-    private String Url;
+    private String name;
+    private String url;
 
     public Person(String url, String name){
-        this.Url = url;
-        this.Name = name;
+        this.url = url;
+        this.name = name;
     }
 
     public String getName() {
-        return Name;
+        return name;
     }
 
     public String getUrl() {
-        return Url;
+        return url;
     }
 
     public void setUrl(String url) {
-        Url = url;
+        url = url;
     }
 
     public void setName(String name) {
-        Name = name;
+        name = name;
     }
 }
